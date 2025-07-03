991626b1acdcc8710faf50ea0c0b6ea9ddb8abc6
sNirajan <snirajan212@gmail.com>
2025-07-03 14:42:42 -0500

    feat: add migration for policies table with relevant fields

---
+++ database/migrations/2025_07_03_193628_create_policies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('policies', function (Blueprint $table) {
+            $table->id();       // auto-increment primary key
+            $table->string('policy_number')->unique();
+            $table->string('customer_name');    
+            $table->decimal('premium_amount', 10, 2); // for money
+            $table->enum('status', ['active', 'cancelled', 'pending']); // for status
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('policies');
+    }
+};
